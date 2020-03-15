1abba1a91582f926e24424a8a0e27c1addc575a4
Rob Mathews <robbis@gmail.com>
2020-03-15 19:45:56 +0000

    added limit to character inventory

--- character_controller.rb
+++ character_controller.rb
@@ -19,10 +19,6 @@ get '/' do
   erb(:index)
 end
 
-# not_found do
-#   'You shouldn\'t be here'
-# end
-
 # CHARACTER ROUTES
 
 get '/character' do
@@ -142,14 +138,21 @@ get '/inventory/:id' do
 end
 
 post '/inventory/:id' do
-  @inventory = Inventory.new({
-    'id' => params[:id],
-    'char_id' => params[:char_id],
-    'item_id' => params[:item_id]
-    })
-  @inventory.update
-  @inventory = Inventory.find_by_id(params[:id])
-  erb(:inventory_update)
+  p current_inventory = Inventory.find_inventory(params[:char_id]).map{|inv|inv}.length
+  if current_inventory >= 10
+    @added = false
+    @inventory = Inventory.find_by_id(params[:id])
+  else
+    @inventory = Inventory.new({
+      'id' => params[:id],
+      'char_id' => params[:char_id],
+      'item_id' => params[:item_id]
+      })
+    @inventory.update
+    @inventory = Inventory.find_by_id(params[:id])
+    @added = true
+  end
+    erb(:inventory_update)
 end
 
 # RACE ROUTES
