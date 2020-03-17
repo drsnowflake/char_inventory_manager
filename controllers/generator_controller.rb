f007ac0d6e6ab55dabaa92e32ca0542dc2586f34
Rob Mathews <robbis@gmail.com>
2020-03-17 23:24:50 +0000

    added home made character name generator

--- controllers/generator_controller.rb
+++ controllers/generator_controller.rb
@@ -12,12 +12,23 @@ post '/generator' do
     
     race_choices = Race.find_other_races(-1)
     role_choices = Role.find_other_roles(-1)
-    params[:req_chars].to_i.times{
+    requested = params[:req_chars].to_i
+    
+    dupe_counter = 0
+    counter = 0
+    until counter == requested
+        generated_name = Character.random_name
+        until Character.find_by_name(generated_name) == nil
+            generated_name = Character.random_name
+            dupe_counter += 1
+        end
         Character.new({
-            'char_name' => Character.random_name,
+            'char_name' => generated_name,
             'race_id' => race_choices.sample['id'].to_i,
             'role_id' => role_choices.sample['id'].to_i
         }).save
-    }
+        counter += 1
+    end
+    p "Difference was #{counter - dupe_counter}"
     redirect :'/'
 end
